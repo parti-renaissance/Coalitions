0ef0449787038fc4725db17e22ed216c222d1646
Bastien Teissier <bastient@theodo.fr>
2021-03-02 12:15:07 +0100

    Update layout design

--- src/pages/Home/Home.style.tsx
+++ src/pages/Home/Home.style.tsx
@@ -5,7 +5,6 @@ export const HomeContainer = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
-  padding: ${getSpacing(10)};
 `;
 HomeContainer.displayName = 'HomeContainer';
 

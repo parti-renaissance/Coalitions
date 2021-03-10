ce8ad4075ab1f3a4fdebb3ed3dba4934176ab541
Gaspard Denis <gaspard.denis.123@gmail.com>
2021-03-10 10:36:46 +0100

    Display text fields on login modal

--- src/components/LoginModal/LoginModal.style.ts
+++ src/components/LoginModal/LoginModal.style.ts
@@ -1,7 +1,7 @@
 import { IconButton } from '@material-ui/core';
 import { Close } from '@material-ui/icons';
 import styled from 'styled-components';
-import { fontSize, colorPalette, media, getSpacing } from 'stylesheet';
+import { fontSize, colorPalette, media, getSpacing, fontWeight, fontFamily } from 'stylesheet';
 
 export const ContentContainer = styled.div`
   display: flex;
@@ -22,4 +22,16 @@ export const StyledCloseButton = styled(IconButton)`
 export const StyledCloseIcon = styled(Close)`
   font-size: ${fontSize.xLarge};
   color: ${colorPalette.greyDark};
+`;
+
+export const Title = styled.div`
+  font-family: ${fontFamily.main};
+  font-size: ${fontSize.mediumLarge};
+  font-weight: ${fontWeight.bold};
+  margin-top: ${getSpacing(5)};
+`;
+
+export const InputFieldWrapper = styled.div`
+  margin-top: ${getSpacing(3)};
+  width: 100%;
 `;
--- src/components/LoginModal/LoginModal.tsx
+++ src/components/LoginModal/LoginModal.tsx
@@ -1,12 +1,21 @@
 import React, { FunctionComponent, forwardRef, ForwardRefRenderFunction } from 'react';
 import { getIsMobile } from '../../services/mobile/mobile';
-import { StyledCloseButton, StyledCloseIcon, ContentContainer } from './LoginModal.style';
+import {
+  StyledCloseButton,
+  StyledCloseIcon,
+  ContentContainer,
+  Title,
+  InputFieldWrapper,
+} from './LoginModal.style';
 import { SlideProps } from '@material-ui/core/Slide';
 import { Dialog, Slide } from '@material-ui/core';
+import { useIntl } from 'react-intl';
+import InputField from 'components/InputField';
 
 interface LoginModalProps {
   isOpened: boolean;
   onClose: () => void;
+  title: string;
 }
 
 const SlideUpComponent: ForwardRefRenderFunction<{}, SlideProps> = (props, ref) => (
@@ -15,8 +24,9 @@ const SlideUpComponent: ForwardRefRenderFunction<{}, SlideProps> = (props, ref) 
 
 const SlideUp = forwardRef<{}, SlideProps>(SlideUpComponent);
 
-const LoginModal: FunctionComponent<LoginModalProps> = ({ isOpened, onClose }) => {
+const LoginModal: FunctionComponent<LoginModalProps> = ({ isOpened, onClose, title }) => {
   const isMobile = getIsMobile();
+  const intl = useIntl();
 
   return (
     <Dialog
@@ -28,6 +38,16 @@ const LoginModal: FunctionComponent<LoginModalProps> = ({ isOpened, onClose }) =
         <StyledCloseButton onClick={onClose}>
           <StyledCloseIcon />
         </StyledCloseButton>
+        <Title>{title}</Title>
+        <InputFieldWrapper>
+          <InputField placeholder={intl.formatMessage({ id: 'login_modal.first-name' })} />
+        </InputFieldWrapper>
+        <InputFieldWrapper>
+          <InputField placeholder={intl.formatMessage({ id: 'login_modal.email-address' })} />
+        </InputFieldWrapper>
+        <InputFieldWrapper>
+          <InputField placeholder={intl.formatMessage({ id: 'login_modal.city-or-country' })} />
+        </InputFieldWrapper>
       </ContentContainer>
     </Dialog>
   );

7dbe86b5def3e9c61ac659b87cd7bc06241c5a68
Bastien Teissier <bastient@theodo.fr>
2021-03-11 12:31:27 +0100

    feat(cause): add cta in cause page

--- src/pages/CauseList/CreateCauseCTA/CreateCauseCTA.tsx
+++ src/pages/CauseList/CreateCauseCTA/CreateCauseCTA.tsx
@@ -1,27 +1,53 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
 import { getNumberOfCauses } from 'redux/Cause/selectors';
+import { DefaultLink as Link } from 'components/Link/Link';
 import {
   CTABlock,
   CTAButton,
   CTAContainer,
   DescriptionText,
   HeadSentence,
+  CauseListButton,
 } from './CreateCauseCTA.style';
 
-export const CreateCauseCTA: React.FunctionComponent = () => {
+import { PATHS } from 'routes';
+import { FormattedMessage } from 'react-intl';
+
+export const CreateCauseCTA: React.FunctionComponent<{ displayLinkToCauseList?: boolean }> = ({
+  displayLinkToCauseList = false,
+}) => {
   const numberOfCauses = useSelector(getNumberOfCauses);
 
   return (
     <CTAContainer>
       <CTABlock>
-        <HeadSentence>Vous aussi, agissez !</HeadSentence>
+        <HeadSentence>
+          <FormattedMessage id="cause-cta.title" />
+        </HeadSentence>
         <DescriptionText>
-          Rejoignez les {numberOfCauses} porteurs de causes déjà mobilisés.
+          <FormattedMessage
+            id="cause-cta.description"
+            values={{
+              numberOfCauses,
+            }}
+          />
         </DescriptionText>
-        <CTAButton variant="contained" color="secondary">
-          Je crée ma cause
+        <CTAButton variant="contained" color="primary">
+          <FormattedMessage id="cause-cta.cause-creation" />
         </CTAButton>
+        {displayLinkToCauseList && (
+          <Link to={PATHS.HOME.url()}>
+            <CauseListButton variant="contained">
+              <FormattedMessage
+                id="cause-cta.go-to-cause-list"
+                values={{
+                  numberOfCauses,
+                }}
+              />
+            </CauseListButton>
+          </Link>
+        )}
       </CTABlock>
     </CTAContainer>
   );

54f52b235a0516e9064afadd64972773ea51e8b2
Gaspard Denis <gaspard.denis.123@gmail.com>
2021-03-15 15:28:21 +0100

    Show city code

--- src/components/LoginModal/lib/useCityAndCountryAutocomplete.ts
+++ src/components/LoginModal/lib/useCityAndCountryAutocomplete.ts
@@ -1,20 +1,34 @@
 import { coalitionApiClient } from 'services/networking/client';
 import { useState } from 'react';
 
-export interface City {
+export enum CityOrCountryType {
+  city = 'city',
+  country = 'country',
+}
+export interface CityOrCountry {
   uuid: string;
   name: string;
+  type: CityOrCountryType;
+  code: string;
 }
+
+export const getCityOrCountryLabel = (cityOrCountry: CityOrCountry) => {
+  let label = cityOrCountry.name;
+  if (cityOrCountry.type === CityOrCountryType.city) {
+    label = `${label} (${cityOrCountry.code.substring(0, 2)})`;
+  }
+  return label;
+};
 
 export const useCityAndCountryAutocomplete = () => {
   const [isFetchingCities, setisFetchingCities] = useState<boolean>(false);
-  const [cities, setCities] = useState<City[]>([]);
+  const [cities, setCities] = useState<CityOrCountry[]>([]);
 
   const fetchCities = async (searchText: string) => {
     setisFetchingCities(true);
     try {
       const { items } = await coalitionApiClient.get(
-        `zones?type[]=city&type[]=country&name=${searchText}`,
+        `zones?type[]=city&type[]=country&page_size=20&name=${searchText}`,
       );
       setCities(items);
     } catch (e) {

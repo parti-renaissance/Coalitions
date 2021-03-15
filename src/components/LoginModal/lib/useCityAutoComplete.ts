565739c851f9fee0f040ad346b6c806d920c6d00
Gaspard Denis <gaspard.denis.123@gmail.com>
2021-03-15 12:27:23 +0100

    call pi

--- src/components/LoginModal/lib/useCityAutoComplete.ts
+++ src/components/LoginModal/lib/useCityAutoComplete.ts
@@ -1,14 +1,33 @@
+import { coalitionApiClient } from 'services/networking/client';
+import { useState } from 'react';
+
 export interface City {
-  id: string;
+  uuid: string;
   name: string;
 }
 
-export const useCityAutoComplete = (): { cities: City[] } => {
+export const useCityAutocomplete = () => {
+  const [isFetchingCities, setisFetchingCities] = useState<boolean>(false);
+  const [cities, setCities] = useState<City[]>([]);
+
+  const fetchCities = async (searchText: string) => {
+    setisFetchingCities(true);
+    try {
+      const { items } = await coalitionApiClient.get(
+        `zones?type[]=city&type[]=country&name=${searchText}`,
+      );
+      setCities(items);
+    } catch (e) {
+      // TODO call error service
+      console.warn(e);
+    } finally {
+      setisFetchingCities(false);
+    }
+  };
+
   return {
-    cities: [
-      { id: '1', name: 'Tours' },
-      { id: '2', name: 'Paris' },
-      { id: '3', name: 'Rouen' },
-    ],
+    fetchCities,
+    isFetchingCities,
+    cities,
   };
 };

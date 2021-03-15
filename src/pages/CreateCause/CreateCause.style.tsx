481c053b9fc2008fdf7a32922dc8b9250787364f
Gaspard Denis <gaspard.denis.123@gmail.com>
2021-03-15 18:47:20 +0100

    init two first sections

--- src/pages/CreateCause/CreateCause.style.tsx
+++ src/pages/CreateCause/CreateCause.style.tsx
@@ -1,6 +1,7 @@
 import styled from 'styled-components';
 import { media, getSpacing } from 'stylesheet';
 import { FULL_WIDTH_BUTTON_HEIGHT } from 'components/Button/Button';
+import InputField from 'components/InputField';
 
 export const Container = styled.div`
   ${media.desktop(`
@@ -15,7 +16,7 @@ export const SubContainer = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
-  padding: ${getSpacing(7)} ${getSpacing(5)} ${FULL_WIDTH_BUTTON_HEIGHT} ${getSpacing(5)};
+  padding: ${getSpacing(7)} ${getSpacing(3)} ${FULL_WIDTH_BUTTON_HEIGHT} ${getSpacing(3)};
   ${media.desktop(`
     max-width: ${getSpacing(136)};
     padding: unset;
@@ -28,5 +29,12 @@ export const TopImage = styled.img`
   ${media.desktop(`
     height: ${getSpacing(33)};
     width: ${getSpacing(33)};
+  `)}
+`;
+
+export const StyledInputDescription = styled(InputField)`
+  margin-top: ${getSpacing(3)};
+  ${media.desktop(`
+    margin-top: ${getSpacing(6)};
   `)}
 `;

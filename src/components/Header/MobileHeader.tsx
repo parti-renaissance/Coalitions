e30af29914c38aa4a1e74db236ebca91e760bb81
Bastien Teissier <bastient@theodo.fr>
2021-03-22 09:06:38 +0100

    fix(header): remove name for mobile to prevent overflow on small devices

--- src/components/Header/MobileHeader.tsx
+++ src/components/Header/MobileHeader.tsx
@@ -1,9 +1,7 @@
-import { DefaultLink } from 'components/Link/Link';
 import React from 'react';
 import { FormattedMessage } from 'react-intl';
-import { PATHS } from 'routes';
 import { oauthUrl } from 'services/networking/auth';
-import { HeaderContainer, HeaderSubContainer, HeaderTitle, LogLink } from './Header.style';
+import { HeaderContainer, HeaderSubContainer, LogLink } from './Header.style';
 
 type MobileHeaderProps = {
   isUserLoggedIn: boolean;
@@ -11,11 +9,6 @@ type MobileHeaderProps = {
 
 export const MobileHeader: React.FC<MobileHeaderProps> = ({ isUserLoggedIn }) => (
   <HeaderContainer>
-    <HeaderSubContainer>
-      <DefaultLink to={PATHS.HOME.url()}>
-        <HeaderTitle>Jemengage</HeaderTitle>
-      </DefaultLink>
-    </HeaderSubContainer>
     <HeaderSubContainer>
       {isUserLoggedIn ? (
         <LogLink>

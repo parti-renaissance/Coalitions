4033f71de4efbf21d48dc9738c401de11e259a3c
Gaspard Denis <gaspard.denis.123@gmail.com>
2021-03-22 12:20:36 +0100

    Update IconAndLabel sizes on desktop

--- src/components/IconAndLabel/IconAndLabel.style.ts
+++ src/components/IconAndLabel/IconAndLabel.style.ts
@@ -1,8 +1,7 @@
 import styled from 'styled-components';
 import { getSpacing, media, colorPalette, fontFamily, fontSize, lineHeight } from 'stylesheet';
 
-const MOBILE_ICON_SIZE = '16px';
-const DESKTOP_ICON_SIZE = '20px';
+const ICON_SIZE = '16px';
 
 export const Container = styled.div`
   display: flex;
@@ -10,13 +9,9 @@ export const Container = styled.div`
 `;
 
 export const Icon = styled.img`
-  height: ${MOBILE_ICON_SIZE};
-  width: ${MOBILE_ICON_SIZE};
+  height: ${ICON_SIZE};
+  width: ${ICON_SIZE};
   margin-right: ${getSpacing(2)};
-  ${media.desktop(`
-    height: ${DESKTOP_ICON_SIZE};
-    width: ${DESKTOP_ICON_SIZE};
-  `)}
 `;
 
 export const Label = styled.div`
@@ -24,7 +19,4 @@ export const Label = styled.div`
   font-size: ${fontSize.p.mobile};
   line-height: ${lineHeight.primary};
   color: ${colorPalette.greyDark};
-  ${media.desktop(`
-    font-size: ${fontSize.input.desktop};
-  `)}
 `;

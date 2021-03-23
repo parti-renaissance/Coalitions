397899d61794450193c7779b26315bda348872c9
Gaspard Denis <gaspard.denis.123@gmail.com>
2021-03-23 10:03:58 +0100

    fetch current user is logged in

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,23 +1,34 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useLocation } from 'react-router';
 import { useLogin } from 'redux/Login/hooks/useLogin';
 import { getIsMobile } from 'services/mobile/mobile';
 import { DesktopHeader } from './DesktopHeader';
 import { MobileHeader } from './MobileHeader';
 import { useFetchCurrentUser } from 'redux/User/hooks/useFetchCurrentUser';
+import useSelector from 'redux/useSelector';
+import { isUserLogged } from 'redux/Login';
+import { getCurrentUser } from 'redux/User/selectors';
 
 const Header: React.FC = () => {
   const { search } = useLocation();
   const [, login] = useLogin();
   const { fetchCurrentUser } = useFetchCurrentUser();
+  const isUserLoggedIn = Boolean(useSelector(isUserLogged));
+  const currentUser = useSelector(getCurrentUser);
+
+  useEffect(() => {
+    if (isUserLoggedIn && currentUser === undefined) {
+      fetchCurrentUser();
+    }
+  }, [isUserLoggedIn, currentUser]);
 
   useEffect(() => {
     const params = new URLSearchParams(search);
     const code = params.get('code');
     if (code !== null) {
-      login({ code }).then(fetchCurrentUser);
+      login({ code });
     }
-  }, [login, search, fetchCurrentUser]);
+  }, [login, search]);
 
   if (getIsMobile()) {
     return <MobileHeader />;

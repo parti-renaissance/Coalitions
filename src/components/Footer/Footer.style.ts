b4d7d38711b3942b7307d03a2568150704c1856c
Gaspard Denis <gaspard.denis.123@gmail.com>
2021-03-26 10:47:49 +0100

    init footer

---
+++ src/components/Footer/Footer.style.ts
@@ -0,0 +1,13 @@
+import { Card, CardContent } from '@material-ui/core';
+import styled, { css } from 'styled-components';
+import {
+  boxShadow,
+  getSpacing,
+  colorPalette,
+  media,
+  borderRadius,
+  fontFamily,
+  fontSize,
+  lineHeight,
+  fonts,
+} from 'stylesheet';
---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,7 @@
+import React, { FunctionComponent } from 'react';
+
+const Footer: FunctionComponent<{}> = () => {
+  return <div>{'coucou'}</div>;
+};
+
+export default Footer;

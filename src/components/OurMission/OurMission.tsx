deddc49a465a036d01e0260c65e28b3442df6143
Gaspard Denis <gaspard.denis.123@gmail.com>
2021-03-30 21:03:48 +0200

    video on create cause presentation

--- src/components/OurMission/OurMission.tsx
+++ src/components/OurMission/OurMission.tsx
@@ -1,14 +1,6 @@
 import React, { FunctionComponent } from 'react';
 import { useIntl } from 'react-intl';
-import {
-  Container,
-  SubContainer,
-  SubTitle,
-  Content,
-  Image,
-  DesktopCreateCauseButton,
-  Title,
-} from './OurMission.style';
+import { Container, SubTitle, Content, DesktopCreateCauseButton, Title } from './OurMission.style';
 import { DefaultLink as Link } from 'components/Link/Link';
 import { PATHS } from 'routes';
 
@@ -17,17 +9,14 @@ const OurMissionHeader: FunctionComponent = () => {
 
   return (
     <Container>
-      <SubContainer>
-        <Title>{intl.formatMessage({ id: 'our_mission.title' })}</Title>
-        <SubTitle>{intl.formatMessage({ id: 'our_mission.our-mission' })}</SubTitle>
-        <Content>{intl.formatMessage({ id: 'our_mission.explanation' })}</Content>
-        <Link to={PATHS.CREATE_CAUSE.url()}>
-          <DesktopCreateCauseButton size="small" variant="contained" color="primary">
-            {intl.formatMessage({ id: 'cause-cta.cause-creation' })}
-          </DesktopCreateCauseButton>
-        </Link>
-      </SubContainer>
-      <Image />
+      <Title>{intl.formatMessage({ id: 'our_mission.title' })}</Title>
+      <SubTitle>{intl.formatMessage({ id: 'our_mission.our-mission' })}</SubTitle>
+      <Content>{intl.formatMessage({ id: 'our_mission.explanation' })}</Content>
+      <Link to={PATHS.CREATE_CAUSE.url()}>
+        <DesktopCreateCauseButton size="small" variant="contained" color="primary">
+          {intl.formatMessage({ id: 'cause-cta.cause-creation' })}
+        </DesktopCreateCauseButton>
+      </Link>
     </Container>
   );
 };
---
+++ src/pages/OurMission/OurMission.style.tsx
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+import { media, defaultMargins } from 'stylesheet';
+
+export const VideoWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  padding: ${defaultMargins.vertical.mobile} ${defaultMargins.horizontal.mobile};
+  ${media.desktop(`
+    padding: ${defaultMargins.vertical.desktop} ${defaultMargins.horizontal.desktop};
+  `)}
+`;

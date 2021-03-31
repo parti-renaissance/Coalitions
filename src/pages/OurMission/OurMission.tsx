911885934ac2e18c607a3cf4d6773caa7383642a
Gaspard Denis <gaspard.denis.123@gmail.com>
2021-03-31 16:43:30 +0200

    hide video on our mission page

--- src/pages/OurMission/OurMission.tsx
+++ src/pages/OurMission/OurMission.tsx
@@ -1,13 +1,12 @@
 import React, { FunctionComponent } from 'react';
 import OurMissionHeader from 'components/OurMission';
-import { CauseDefinition } from 'components/Definition/CauseDefinition';
 import CreationSteps from './components/CreationSteps';
 import FixedBottomButton from 'components/FixedBottomButton';
 import { PATHS } from 'routes';
 import { FormattedMessage } from 'react-intl';
 import { useHistory } from 'react-router';
-import { VideoWrapper } from './OurMission.style';
-import Video from 'components/Video';
+// import { VideoWrapper } from './OurMission.style';
+// import Video from 'components/Video';
 
 const OurMission: FunctionComponent = () => {
   const history = useHistory();
@@ -19,10 +18,9 @@ const OurMission: FunctionComponent = () => {
   return (
     <>
       <OurMissionHeader />
-      <VideoWrapper>
+      {/* <VideoWrapper>
         <Video videoId="KD6egRRsZ_Q" />
-      </VideoWrapper>
-      <CauseDefinition />
+      </VideoWrapper> */}
       <CreationSteps />
       <FixedBottomButton onClick={onCreateCauseClick}>
         <FormattedMessage id="cause-cta.cause-creation" />

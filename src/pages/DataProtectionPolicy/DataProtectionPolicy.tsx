200f96afa4dba02f0076604231943e893afa80b6
Gaspard Denis <gaspard.denis.123@gmail.com>
2021-03-31 18:11:34 +0200

    init legal pages

---
+++ src/pages/DataProtectionPolicy/DataProtectionPolicy.tsx
@@ -0,0 +1,8 @@
+import React, { FunctionComponent } from 'react';
+import LegalPage from 'components/LegalPage';
+
+const DataProtectionPolicy: FunctionComponent = () => (
+  <LegalPage titleKey="data_protection_policy.title" contents={[]} />
+);
+
+export default DataProtectionPolicy;
---
+++ src/pages/LegalNotice/LegalNotice.tsx
@@ -0,0 +1,8 @@
+import React, { FunctionComponent } from 'react';
+import LegalPage from 'components/LegalPage';
+
+const LegalNotice: FunctionComponent = () => (
+  <LegalPage titleKey="legal_notice.title" contents={[]} />
+);
+
+export default LegalNotice;

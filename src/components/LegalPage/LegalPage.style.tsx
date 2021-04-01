7fb4ae42dbe92569346c6029b33be553ae365748
Gaspard Denis <gaspard.denis.123@gmail.com>
2021-04-01 10:30:05 +0200

    cookies policy content

--- src/components/LegalPage/LegalPage.style.tsx
+++ src/components/LegalPage/LegalPage.style.tsx
@@ -1,4 +1,4 @@
-import { getSpacing, colorPalette, fonts, media, defaultMargins, fontFamily } from 'stylesheet';
+import { getSpacing, colorPalette, fonts, media, defaultMargins } from 'stylesheet';
 import styled from 'styled-components';
 
 export const Container = styled.div`
@@ -6,30 +6,30 @@ export const Container = styled.div`
     ${defaultMargins.horizontal.mobile};
   ${media.desktop(`
     padding: ${defaultMargins.vertical.desktop} ${defaultMargins.horizontal.desktop};
-    max-width: ${getSpacing(136)};
+    max-width: ${getSpacing(140)};
     margin: 0 auto;
   `)}
+  h2 {
+    margin-top: ${getSpacing(6)};
+  }
+  h3 {
+    margin-top: ${getSpacing(4)};
+  }
+  p {
+    margin-top: ${getSpacing(3)};
+  }
+  ul {
+    ${fonts.p};
+    list-style: disc;
+    margin-left: ${getSpacing(4)};
+    li {
+      margin-top: ${getSpacing(3)};
+    }
+  }
 `;
 
 export const Title = styled.h1`
   ${fonts.h1Small};
   color: ${colorPalette.greyDark};
-`;
-
-export const SubTitle = styled.div`
-  ${fonts.h2};
-  font-family: ${fontFamily.secondary};
-  margin-top: ${getSpacing(6)};
-  color: ${colorPalette.greyDark};
-`;
-
-export const ArticleTitle = styled.div`
-  ${fonts.h2};
-  margin-top: ${getSpacing(4)};
-  color: ${colorPalette.greyDark};
-`;
-
-export const Text = styled.p`
-  margin-top: ${getSpacing(3)};
-  color: ${colorPalette.greyDark};
+  margin-bottom: ${getSpacing(3)};
 `;
--- src/pages/CookiesPolicy/CookiesPolicy.tsx
+++ src/pages/CookiesPolicy/CookiesPolicy.tsx
@@ -2,7 +2,266 @@ import React, { FunctionComponent } from 'react';
 import LegalPage from 'components/LegalPage';
 
 const CookiesPolicy: FunctionComponent = () => (
-  <LegalPage titleKey="cookies_policy.title" contents={[]} />
+  <LegalPage
+    title="POLITIQUE D&rsquo;UTILISATION DES COOKIES SUR LA PLATEFORME &laquo;&#8239;POUR UNE
+  CAUSE&#8239;&raquo;"
+  >
+    <p>
+      La pr&eacute;sente Politique d&rsquo;utilisation des cookies a vocation &agrave;
+      pr&eacute;ciser les types de Cookies utilis&eacute;s sur la Plateforme &laquo;&#8239;Pour une
+      Cause&#8239;&raquo; &eacute;dit&eacute;e par La R&eacute;publique en Marche (ci-apr&egrave;s
+      &laquo;&#8239;LaREM&#8239;&raquo;) et leurs finalit&eacute;s d&rsquo;utilisation.
+    </p>
+    <h2>Article 1. D&eacute;finitions </h2>
+    <p>
+      Cookies&#8239;: d&eacute;signent les fichiers texte non ex&eacute;cutables qui sont
+      d&eacute;pos&eacute;s et stock&eacute;s par le serveur d&rsquo;un site web sur le Terminal de
+      l&rsquo;Utilisateur de la Plateforme ;
+    </p>
+    <p>
+      RGPD&#8239;: d&eacute;signe le R&egrave;glement (UE) 2016/679 du Parlement europ&eacute;en et
+      du Conseil du 27 avril 2016, relatif &agrave; la protection des personnes physique &agrave;
+      l&rsquo;&eacute;gard du traitement des donn&eacute;es &agrave; caract&egrave;re personnel et
+      &agrave; la libre circulation de ces donn&eacute;es dit R&egrave;glement G&eacute;n&eacute;ral
+      sur la Protection des Donn&eacute;es&#8239;;
+    </p>
+    <p>
+      Services&#8239;: d&eacute;signent l&rsquo;ensemble des fonctionnalit&eacute;s et contenus
+      accessibles sur la Plateforme&#8239;;
+    </p>
+    <p>
+      Plateforme&#8239;: d&eacute;signe le site web &laquo;&#8239;Pour une Cause&#8239;&raquo;
+      d&eacute;di&eacute; &agrave; la cr&eacute;ation et l&rsquo;animation de causes,
+      d&eacute;ploy&eacute;e par LaREM et accessible &agrave; l&rsquo;adresse url pourunecause.fr ;
+    </p>
+    <p>
+      Terminal&#8239;: d&eacute;signe l&rsquo;appareil par lequel l&rsquo;Utilisateur navigue sur le
+      web (ordinateur, smartphone, tablette etc.)&#8239;;
+    </p>
+    <p>
+      Tiers&#8239;: d&eacute;signent les personnes physiques ou morales distinctes de LaREM dont la
+      technologie est utilis&eacute;e par la Plateforme ou vers lequel un lien est accessible sur la
+      Plateforme&#8239;;
+    </p>
+    <p>
+      Utilisateur&#8239;: d&eacute;signe toute personne acc&eacute;dant et/ou utilisant les Services
+      de la Plateforme.
+    </p>
+    <h2>Article 2 : &Agrave; propos des cookies </h2>
+    <p>
+      Lorsque l&rsquo;Utilisateur visite la Plateforme, des Cookies peuvent &ecirc;tre
+      d&eacute;pos&eacute;s sur son Terminal. Ces Cookies peuvent impliquer la transmission
+      d&rsquo;informations, soit entre lui et LaREM, soit &agrave; destination d&rsquo;un tiers pour
+      le compte de LaREM ou en accord avec la propre politique de protection de la vie priv&eacute;e
+      de ces Tiers.
+    </p>
+    <h2>Article 3 : Finalit&eacute; des cookies utilis&eacute;s</h2>
+    <p>
+      LaREM utilise divers types de Cookies pour mieux comprendre la fa&ccedil;on dont les Visiteurs
+      et Utilisateurs utilisent les Services propos&eacute;s sur la Plateforme et ainsi
+      am&eacute;liorer le fonctionnement et les Services de la Plateforme. La m&eacute;morisation de
+      Cookies sur le Terminal de l&rsquo;Utilisateur permet &agrave; LaREM notamment de
+      personnaliser la navigation de l&rsquo;Utilisateur sur la Plateforme et de rendre sa prochaine
+      visite plus agr&eacute;able et intuitive.
+    </p>
+    <p>Les Cookies &eacute;mis sur cette Plateforme ont de multiples usages&#8239;: </p>
+    <h3>3.1. Cookies strictement n&eacute;cessaires &agrave; la Plateforme&#8239;: </h3>
+    <p>
+      Ces Cookies permettent d&rsquo;enregistrer des informations entre deux consultations de la
+      Plateforme sur un m&ecirc;me Terminal. Ils sont utilis&eacute;s pour&#8239;:
+    </p>
+    <ul>
+      <li>fournir le contenu de la Plateforme&#8239;;</li>
+      <li>mettre en &#339;uvre des mesures de s&eacute;curit&eacute; sur la Plateforme&#8239;;</li>
+      <li>
+        g&eacute;rer les autorisations d&rsquo;utilisations de Cookies sur la Plateforme&#8239;;
+      </li>
+      <li>identifier l&rsquo;acc&egrave;s de robot &agrave; la Plateforme&#8239;;</li>
+      <li>
+        faciliter la navigation de l&rsquo;Utilisateur sur la Plateforme (par exemple en permettant
+        au serveur de reconna&icirc;tre l&rsquo;Utilisateur en toute s&eacute;curit&eacute;, et sans
+        qu&rsquo;il ait donc besoin de s&rsquo;identifier &agrave; chaque changement de
+        page)&#8239;;
+      </li>
+      <li>
+        m&eacute;moriser des informations relatives &agrave; un formulaire que vous avez rempli sur
+        la Plateforme (inscription ou messages envoy&eacute;s via les formulaires ad&eacute;quats) ;
+      </li>
+      <li>
+        permettre aux Utilisateurs d'acc&eacute;der &agrave; des espaces personnels sur la
+        Plateforme, tel que l'historique de navigation&#8239;;
+      </li>
+    </ul>
+    <p>
+      Conform&eacute;ment &agrave; la r&egrave;glementation en vigueur et notamment l&rsquo;article
+      82 de la loi n&deg;78-17 du 6 janvier 1978 relative &agrave; l&rsquo;informatique, aux
+      fichiers et aux libert&eacute;s, ces Cookies sont exempt&eacute;s du recueil du consentement
+      de l&rsquo;Utilisateur puisqu&rsquo;ils sont n&eacute;cessaires &agrave; la navigation sur la
+      Plateforme et/ou strictement n&eacute;cessaires &agrave; la fourniture d&rsquo;un Service
+      express&eacute;ment demand&eacute; par l&rsquo;Utilisateur.
+    </p>
+    <h3>
+      3.2. Cookies de fonctionnalit&eacute;s&#8239;: &laquo;&#8239;Exp&eacute;riences et
+      relations&#8239;&raquo;
+    </h3>
+    <p>
+      Ils sont d&eacute;pos&eacute;s par la Plateforme en plus des cookies n&eacute;cessaires afin
+      de&#8239;permettre la personnalisation de la navigation de l&rsquo;Utilisateur sur la
+      Plateforme :
+    </p>
+    <ul>
+      <li>
+        suivre et m&eacute;moriser le comportement de l&rsquo;Utilisateur et conserver ses
+        pr&eacute;f&eacute;rences&#8239;;
+      </li>
+      <li>
+        enregistrer des &eacute;l&eacute;ments de personnalisation d&rsquo;interface
+        effectu&eacute;e par l&rsquo;Utilisateur&#8239;;
+      </li>
+      <li>fluidifier l&rsquo;acc&egrave;s au contenu de la Plateforme&#8239;; </li>
+      <li>
+        de relier les contenus consult&eacute;s sur la Plateforme et le compte de r&eacute;seau
+        social de l&rsquo;Utilisateur lorsqu&rsquo;il est connect&eacute; &agrave; un de ces
+        r&eacute;seaux sociaux lors de sa navigation sur la Plateforme.
+      </li>
+    </ul>
+    <p>
+      La base l&eacute;gale de l&rsquo;utilisation de ces Cookies est le consentement. Ce
+      consentement peut &ecirc;tre retir&eacute; &agrave; tout moment.
+    </p>
+    <h3>3.3. Cookies de statistiques ou d&rsquo;audience </h3>
+    <p>
+      La R&eacute;publique en Marche utilise des cookies pour&#8239;conna&icirc;tre
+      l&rsquo;utilisation et les performances de sa Plateforme et ainsi adapter et am&eacute;liorer
+      l&rsquo;int&eacute;r&ecirc;t et l&rsquo;ergonomie des Services de la Plateforme ainsi que
+      leurs contenus. Ces Cookies sont utilis&eacute;s pour&#8239;:
+    </p>
+    <ul>
+      <li>
+        enregistrer des donn&eacute;es statistiques sur l&rsquo;utilisation de la Plateforme ;
+      </li>
+      <li>
+        &eacute;tablir des statistiques sur les volumes de fr&eacute;quentation et
+        d&rsquo;utilisation des divers &eacute;l&eacute;ments de la Plateforme&#8239;;
+      </li>
+    </ul>
+    <p>
+      La base l&eacute;gale de l&rsquo;utilisation de ces Cookies est le consentement. Ce
+      consentement peut &ecirc;tre retir&eacute; &agrave; tout moment.
+    </p>
+    <h3>3.4. Cookies tiers et Cookies d&rsquo;annonces personnalis&eacute;es </h3>
+    <p>
+      Les cookies tiers sont des Cookies d&eacute;pos&eacute;s sur le Terminal de
+      l&rsquo;Utilisateur par des entit&eacute;s tierces, lorsque l&rsquo;Utilisateur acc&egrave;de
+      &agrave; un service de la Plateforme utilisant une technologie tierce ou lorsqu&rsquo;il
+      clique sur les ic&ocirc;nes ou sur les liens tiers accessibles sur la Plateforme.
+    </p>
+    <p>
+      En effet, la Plateforme utilise des technologies tierces pour fournir certains Services. Il
+      contient &eacute;galement des liens de partage par email ou vers des r&eacute;seaux sociaux
+      (WhatsApp, Facebook, Twitter, LinkedIn, YouTube etc.). En cliquant sur ces liens, ou en
+      acc&eacute;dant &agrave; un service de la Plateforme utilisant une technologie tierce, un
+      Cookie tiers est d&eacute;pos&eacute; sur le Terminal de l&rsquo;Utilisateur. Ces Cookies
+      peuvent permettre :
+    </p>
+    <ul>
+      <li>&agrave; LaREM d&rsquo;identifier le Terminal des Utilisateurs r&eacute;currents ; </li>
+      <li>
+        &agrave; LaREM et aux Tiers de collecter des donn&eacute;es sur l&rsquo;utilisation de la
+        Plateforme par l&rsquo;Utilisateur&#8239;;
+      </li>
+      <li>
+        de stocker des donn&eacute;es relatives aux sites internet que vous avez
+        visit&eacute;&#8239;;
+      </li>
+      <li>aux Tiers de mesurer l&rsquo;audience de la Plateforme&#8239;; </li>
+      <li>
+        de partager des contenus de la Plateforme avec des tierces personnes via les r&eacute;seaux
+        sociaux ou d&rsquo;autres supports&#8239;;
+      </li>
+      <li>de fournir des annonces cibl&eacute;es aux Utilisateurs ; </li>
+      <li>
+        de vous permettre de faire conna&icirc;tre votre opinion &agrave; d&rsquo;autres personnes
+        du site tiers choisi&#8239;(ex&#8239;: lorsque vous &laquo;&#8239;aimez&#8239;&raquo; des
+        publications, &laquo;&#8239;suivez&#8239;&raquo; des personnes sur les r&eacute;seaux
+        sociaux, etc.) ;
+      </li>
+    </ul>
+    <p>
+      LaREM ne contr&ocirc;le la collecte par les sites web tiers des informations relatives
+      &agrave; la navigation de l&rsquo;Utilisateur sur la Plateforme ni les informations
+      associ&eacute;es aux donn&eacute;es personnelles dont ils disposent. Il est recommand&eacute;
+      de consulter les politiques d&rsquo;utilisation des cookies de ces sites tiers afin de prendre
+      connaissance des finalit&eacute;s d&rsquo;utilisation des cookies par ces derniers.
+    </p>
+    <h2>Article 4. Activation/d&eacute;sactivation des cookies</h2>
+    <p>
+      Conform&eacute;ment &agrave; l&rsquo;article 82 de la loi n&deg;78-17 du 6 janvier 1978
+      relative &agrave; l&rsquo;informatique, aux fichiers et aux libert&eacute;s, au RGPD et
+      &agrave; l&rsquo;article 5-3 de la directive 2002/58/CE telle que modifi&eacute;e en 2009, le
+      consentement pr&eacute;alable de l&rsquo;Utilisateur doit &ecirc;tre recueilli avant le
+      stockage d&rsquo;informations sur son Terminal ou l&rsquo;acc&egrave;s &agrave; des
+      informations d&eacute;j&agrave; stock&eacute;s sur celui-ci&#8239;; sauf si les cookies sont
+      strictement n&eacute;cessaires &agrave; la fourniture d&rsquo;un Service de communication en
+      ligne express&eacute;ment demand&eacute; par l&rsquo;Utilisateur ou ont une finalit&eacute;
+      exclusive de permettre ou faciliter une communication par voie &eacute;lectronique.
+    </p>
+    <p>
+      Le consentement est d&eacute;fini par le RGPD en son article 4. Il doit &ecirc;tre libre,
+      sp&eacute;cifique, &eacute;clair&eacute; et univoque. Il doit se manifester par un acte
+      positif.
+    </p>
+    <p>
+      Ainsi, LaREM met &agrave; disposition de l&rsquo;Utilisateur un bandeau relatif aux Cookies
+      qui appara&icirc;t lors de sa connexion &agrave; la Plateforme. Ce bandeau pr&eacute;sente les
+      informations sur l&rsquo;utilisation des cookies sur la Plateforme et les moyens permettant
+      &agrave; l&rsquo;Utilisateur de choisir d&rsquo;activer ou non les Cookies selon leur type et
+      de donner son consentement.
+    </p>
+    <p>
+      L&rsquo;Utilisateur est inform&eacute; que le rejet complet des fichiers Cookies peut
+      l&rsquo;emp&ecirc;cher de profiter pleinement des fonctionnalit&eacute;s offertes sur la
+      Plateforme.
+    </p>
+    <p>
+      Les Utilisateurs peuvent &agrave; tout moment r&eacute;activer ou d&eacute;sactiver les types
+      de Cookies de son choix depuis son profil.
+    </p>
+    <h2>Article 5. Dur&eacute;e de conservation</h2>
+    <p>
+      Les Cookies sont conserv&eacute;s pour la p&eacute;riode de validit&eacute; du consentement du
+      Visiteur ou de l&rsquo;Utilisateur au d&eacute;p&ocirc;t des Cookies sur son terminal.
+    </p>
+    <p>
+      Dans tous les cas, LaREM s&rsquo;assure que la dur&eacute;e de validit&eacute; des cookies
+      n&rsquo;exc&egrave;de pas six (6) mois.
+    </p>
+    <p>
+      La p&eacute;riode de validit&eacute; du consentement au d&eacute;p&ocirc;t de Cookies sur le
+      terminal du Visiteur ou de l&rsquo;Utilisateur varie. Elle correspond &agrave;&#8239;:
+    </p>
+    <ul>
+      <li>
+        la dur&eacute;e de visite ou d&rsquo;utilisation pour les Cookies de session et&#8239;;
+      </li>
+      <li>6 mois &agrave; partir de leur activation pour les autres Cookies. </li>
+    </ul>
+    <h2>Article 6. Mise &agrave; jour de la politique d'utilisation des cookies</h2>
+    <p>
+      LaREM se r&eacute;serve le droit d&rsquo;apporter tous changements et corrections &agrave; la
+      pr&eacute;sente Politique d&rsquo;utilisation des Cookies.
+    </p>
+    <p>
+      L&rsquo;Utilisateur sera inform&eacute; des &eacute;ventuelles modifications via le bandeau
+      relatif aux Cookies. Il est &eacute;galement invit&eacute; &agrave; consulter
+      r&eacute;guli&egrave;rement la pr&eacute;sente page.
+    </p>
+    <p>
+      Pour en savoir plus sur vos droits et le traitement de vos donn&eacute;es par LaREM, veuillez
+      consulter la Politique de protection des donn&eacute;es personnelles dans le cadre de cette
+      Plateforme, en cliquant ici
+    </p>
+  </LegalPage>
 );
 
 export default CookiesPolicy;

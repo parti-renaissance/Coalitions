9092c8721cd332a8fcebb8937f62abf0605d8ce5
Gaspard D <gaspard.denis.123@gmail.com>
2021-04-01 16:58:30 +0200

    hide create cause button on our mission page (#235)

--- src/components/Header/DesktopHeader.tsx
+++ src/components/Header/DesktopHeader.tsx
@@ -11,33 +11,41 @@ import {
 } from './Header.style';
 import { MediumLargeButton } from 'components/Button/Button';
 import LogInOrOutButton from './components/LogInOrOutButton';
+import { useLocation } from 'react-router';
 
-export const DesktopHeader: FunctionComponent<{}> = () => (
-  <HeaderContainer>
-    <HeaderSubContainer>
-      <DefaultLink to={PATHS.HOME.url()}>
-        <HeaderTitle>
-          <FormattedMessage id="header.app-name" />
-        </HeaderTitle>
-      </DefaultLink>
-      <DefaultLink to={PATHS.CAUSE_LIST.url()}>
-        <SubCategory>
-          <FormattedMessage id="header.causes" />
-        </SubCategory>
-      </DefaultLink>
-      <DefaultHashLink to={`${PATHS.HOME.url()}#coalitions`}>
-        <SubCategory>
-          <FormattedMessage id="header.coalitions" />
-        </SubCategory>
-      </DefaultHashLink>
-    </HeaderSubContainer>
-    <HeaderSubContainer>
-      <CreateCauseButton to={PATHS.OUR_MISSION.url()}>
-        <MediumLargeButton variant="contained" color="primary">
-          <FormattedMessage id="header.cause-creation" />
-        </MediumLargeButton>
-      </CreateCauseButton>
-      <LogInOrOutButton />
-    </HeaderSubContainer>
-  </HeaderContainer>
-);
+export const DesktopHeader: FunctionComponent<{}> = () => {
+  const location = useLocation();
+  const showCreateCauseButton = !location.pathname.includes(PATHS.OUR_MISSION.url());
+
+  return (
+    <HeaderContainer>
+      <HeaderSubContainer>
+        <DefaultLink to={PATHS.HOME.url()}>
+          <HeaderTitle>
+            <FormattedMessage id="header.app-name" />
+          </HeaderTitle>
+        </DefaultLink>
+        <DefaultLink to={PATHS.CAUSE_LIST.url()}>
+          <SubCategory>
+            <FormattedMessage id="header.causes" />
+          </SubCategory>
+        </DefaultLink>
+        <DefaultHashLink to={`${PATHS.HOME.url()}#coalitions`}>
+          <SubCategory>
+            <FormattedMessage id="header.coalitions" />
+          </SubCategory>
+        </DefaultHashLink>
+      </HeaderSubContainer>
+      <HeaderSubContainer>
+        {showCreateCauseButton && (
+          <CreateCauseButton to={PATHS.OUR_MISSION.url()}>
+            <MediumLargeButton variant="contained" color="primary">
+              <FormattedMessage id="header.cause-creation" />
+            </MediumLargeButton>
+          </CreateCauseButton>
+        )}
+        <LogInOrOutButton />
+      </HeaderSubContainer>
+    </HeaderContainer>
+  );
+};

edeaa86be47c5b33d9b46af16d0c24f5c9b1ed72
Gaspard Denis <gaspard.denis.123@gmail.com>
2021-04-21 09:28:36 +0200

    share button on coalition page

--- src/pages/Coalition/Coalition.style.ts
+++ src/pages/Coalition/Coalition.style.ts
@@ -7,6 +7,18 @@ import {
   defaultMargins,
   contentMaxWidth,
 } from 'stylesheet';
+import { DESKTOP_BUTTONS_WIDTH } from 'components/CauseDetails/components/HeaderButtons/HeaderButtons.style';
+
+export const HeaderContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: ${getSpacing(3)} ${defaultMargins.horizontal.mobile};
+  ${media.desktop(`
+    padding: ${getSpacing(8)} ${defaultMargins.horizontal.desktop};
+    background-color: ${colorPalette.greyLight};
+  `)}
+`;
 
 export const Image = styled.img`
   width: 100%;
@@ -32,5 +44,11 @@ export const ContentContainer = styled.div`
 export const DescriptionWrapper = styled.div`
   ${media.desktop(`
     max-width: ${contentMaxWidth};
+  `)};
+`;
+
+export const HeaderSubContainer = styled.div`
+  ${media.desktop(`
+    width: ${DESKTOP_BUTTONS_WIDTH};
   `)};
 `;

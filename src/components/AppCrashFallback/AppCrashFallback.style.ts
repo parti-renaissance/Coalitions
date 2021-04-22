30c7ee27077c1f85c511c6ced60634d88a129e42
Gaspard D <gaspard.denis.123@gmail.com>
2021-04-22 18:23:55 +0200

    design app crash fallback (#334)
    * design app crash fallback
    
    * fix tests

--- src/components/AppCrashFallback/AppCrashFallback.style.ts
+++ src/components/AppCrashFallback/AppCrashFallback.style.ts
@@ -1,24 +1,60 @@
 import styled from 'styled-components';
-import { getSpacing } from 'stylesheet';
-import BaseButton from 'components/Button';
+import { defaultMargins, getSpacing, media, styledTags } from 'stylesheet';
+import { MediumLargeButton } from 'components/Button/Button';
+
+export const Wrapper = styled.main`
+  height: 100%;
+`;
 
 export const Container = styled.div`
+  ${styledTags};
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
   display: flex;
-  justify-content: center;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-between;
+  padding: ${getSpacing(3)} ${defaultMargins.horizontal.mobile};
+  ${media.desktop(`
+    padding: ${defaultMargins.vertical.desktop} ${defaultMargins.horizontal.desktop};
+  `)}
 `;
-Container.displayName = 'Container';
 
-export const PageContent = styled.div`
-  padding: ${getSpacing(8)} ${getSpacing(4)};
+export const SubContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
 `;
-PageContent.displayName = 'PageContent';
 
-export const HelperList = styled.ul`
-  list-style: disc inside;
-  margin-top: ${getSpacing(2)};
+const IMAGE_WIDTH = getSpacing(51);
+const IMAGE_HEIGHT = getSpacing(45);
+
+export const Image = styled.img`
+  width: ${IMAGE_WIDTH};
+  height: ${IMAGE_HEIGHT};
 `;
-HelperList.displayName = 'HelperList';
 
-export const Button = styled(BaseButton)`
-  padding: ${getSpacing(1)} ${getSpacing(2)};
+export const Description = styled.p`
+  font-style: italic;
+  margin-top: ${getSpacing(12)};
+  text-align: center;
+  max-width: ${getSpacing(120)};
+`;
+
+export const Button = styled(MediumLargeButton)`
+  width: 100%;
+  :last-child {
+    margin-top: ${getSpacing(3)};
+  }
+
+  ${media.desktop(`
+    width: ${getSpacing(60)};
+    :last-child {
+      margin-top: unset;
+      margin-left: ${getSpacing(3)};
+    }
+  `)}
 `;

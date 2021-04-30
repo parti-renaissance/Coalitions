6fbd3b70cddae3f615408c48cf778554f1708612
Gaspard Denis <gaspard.denis.123@gmail.com>
2021-04-30 10:34:08 +0200

    init menu style fix

--- src/components/ShareButton/ShareButton.style.tsx
+++ src/components/ShareButton/ShareButton.style.tsx
@@ -1,6 +1,7 @@
 import { Menu } from '@material-ui/core';
 import styled from 'styled-components';
-import { fonts, getSpacing } from 'stylesheet';
+import { boxShadow, fonts, getSpacing } from 'stylesheet';
+import { DESKTOP_BUTTONS_WIDTH } from 'components/CauseDetails/components/HeaderButtons/HeaderButtons.style';
 
 export const MobileShareIcon = styled.img`
   height: ${getSpacing(5)};
@@ -8,8 +9,16 @@ export const MobileShareIcon = styled.img`
 `;
 
 export const ShareMenu = styled(Menu)`
-  margin-top: ${getSpacing(10)};
-  li {
+  margin-top: ${getSpacing(12)};
+  a {
     ${fonts.input};
+    display: flex;
+    justify-content: center;
+    padding: ${getSpacing(2)};
+    text-align: center;
+  }
+  .MuiMenu-paper {
+    width: ${DESKTOP_BUTTONS_WIDTH};
+    box-shadow: ${boxShadow.card};
   }
 `;

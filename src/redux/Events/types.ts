51fd947ad9a7fd1f861dd2e0dbdc9bf1678fbef3
Gaspard Denis <gaspard.denis.123@gmail.com>
2021-05-13 16:48:57 +0200

    complete event type

--- src/redux/Events/types.ts
+++ src/redux/Events/types.ts
@@ -1,8 +1,7 @@
-export interface EventType {
-  uuid: string;
+export interface InCreationEventType {
   begin_at: Date;
   finish_at: Date;
-  capacity: number;
+  capacity: number | null;
   category: {
     event_group_category: {
       name: string;
@@ -11,4 +10,25 @@ export interface EventType {
   mode: 'meeting' | 'online';
   name: string;
   time_zone: string;
+  description?: string;
+  organizer?: {
+    first_name: string;
+    last_name: string;
+  };
+  participants_count?: number;
+  status?: string;
+  post_address?: {
+    address: string;
+    postal_code: string;
+    city: string;
+    city_name: string;
+    country: string;
+    latitude: number;
+    longitude: number;
+  };
+  visio_url: string | null;
 }
+
+export type EventType = {
+  uuid: string;
+} & InCreationEventType;

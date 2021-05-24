7092d6d992eee49926fddaf097a383263543e418
Gaspard Denis <gaspard.denis.123@gmail.com>
2021-05-24 14:18:50 +0200

    init information component

--- src/components/EventDetailsModal/components/EventInformation/EventInformation.style.ts
+++ src/components/EventDetailsModal/components/EventInformation/EventInformation.style.ts
@@ -1,5 +1,5 @@
 import styled from 'styled-components';
-import { colorPalette, media, getSpacing } from 'stylesheet';
+import { colorPalette, media, getSpacing, fonts } from 'stylesheet';
 
 export const Container = styled.div`
   background-color: ${colorPalette.greyLight};
@@ -9,5 +9,30 @@ export const Container = styled.div`
     min-width: ${getSpacing(64)};
     margin: -64px -${getSpacing(8)} -${getSpacing(8)} ${getSpacing(8)};
     padding: 64px ${getSpacing(8)} ${getSpacing(8)} ${getSpacing(8)};
+  `)}
+`;
+
+export const DesktopButtonsContainer = styled.div`
+  display: none;
+  ${media.desktop(`
+    display: flex;
+  `)}
+`;
+
+export const MobileButtonsContainer = styled.div`
+  margin-top: ${getSpacing(6)};
+  ${media.desktop(`
+    display: none;
+  `)}
+`;
+
+export const SectionTitle = styled.div`
+  ${fonts.h3};
+  color: ${colorPalette.greyDark};
+  :not(:nth-child(2)) {
+    margin-top: ${getSpacing(6)};
+  }
+  ${media.desktop(`
+    margin-top: ${getSpacing(6)};
   `)}
 `;
--- src/components/EventDetailsModal/components/EventInformation/EventInformation.tsx
+++ src/components/EventDetailsModal/components/EventInformation/EventInformation.tsx
@@ -1,16 +1,35 @@
 import React, { FunctionComponent } from 'react';
 import { EventType } from 'redux/Events/types';
-import { Container } from './EventInformation.style';
+import {
+  Container,
+  SectionTitle,
+  MobileButtonsContainer,
+  DesktopButtonsContainer,
+} from './EventInformation.style';
 import ShareButton from '../../../ShareButton';
+import { useIntl } from 'react-intl';
 
 interface EventInformationProps {
   event: EventType;
 }
 
+const ShareEventButton = ({ event }: { event: EventType }) => (
+  <ShareButton shareContent={{ title: event.name, text: event.name }} />
+);
+
 const EventInformation: FunctionComponent<EventInformationProps> = ({ event }) => {
+  const intl = useIntl();
+
   return (
     <Container>
-      <ShareButton shareContent={{ title: event.name, text: event.name }} />
+      <DesktopButtonsContainer>
+        <ShareEventButton event={event} />
+      </DesktopButtonsContainer>
+      <SectionTitle>{intl.formatMessage({ id: 'event_details.information' })}</SectionTitle>
+      <SectionTitle>{intl.formatMessage({ id: 'event_details.add_to_calendar' })}</SectionTitle>
+      <MobileButtonsContainer>
+        <ShareEventButton event={event} />
+      </MobileButtonsContainer>
     </Container>
   );
 };

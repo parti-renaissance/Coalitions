bfff3daadf931151b0b7f4f728d328bd9cff4376
Gaspard Denis <gaspard.denis.123@gmail.com>
2021-06-04 10:09:35 +0200

    update is upcoming event method

--- src/redux/Events/helpers/isUpcomingEvent.ts
+++ src/redux/Events/helpers/isUpcomingEvent.ts
@@ -3,7 +3,7 @@ import { isBefore } from 'date-fns';
 import { zonedTimeToUtc } from 'date-fns-tz';
 
 export const isUpcomingEvent = (event: EventType): boolean => {
-  const beginAt = zonedTimeToUtc(event.beginAt, event.timeZone);
+  const finishAt = zonedTimeToUtc(event.finishAt, event.timeZone);
 
-  return isBefore(new Date(), beginAt);
+  return isBefore(new Date(), finishAt);
 };

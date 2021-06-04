17c1a66aeff38750042d3ef8a233d1ffaf759bad
Gaspard Denis <gaspard.denis.123@gmail.com>
2021-06-04 18:41:21 +0200

    enable to fetch event if not logged

--- src/redux/Events/hooks/useFetchEvent.ts
+++ src/redux/Events/hooks/useFetchEvent.ts
@@ -1,6 +1,5 @@
 import { useCallback, useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { isUserLogged } from 'redux/Login';
+import { useDispatch } from 'react-redux';
 import { useTypedAsyncFn } from 'redux/useTypedAsyncFn';
 import HandleErrorService from 'services/HandleErrorService';
 import { coalitionApiClient } from 'services/networking/client';
@@ -9,7 +8,6 @@ import { updateOneEvent } from '../slice';
 import { RawEventType } from '../types';
 
 export const useFetchEvent = (id: string) => {
-  const isUserLoggedIn = Boolean(useSelector(isUserLogged));
   const dispatch = useDispatch();
 
   const [{ loading, error }, doFetchEvent] = useTypedAsyncFn(async () => {
@@ -23,17 +21,13 @@ export const useFetchEvent = (id: string) => {
   }, [error]);
 
   const fetchEvent = useCallback(async () => {
-    if (!isUserLoggedIn) {
-      return;
-    }
-
     const rawEvent: RawEventType | undefined = await doFetchEvent();
     if (rawEvent === undefined || rawEvent instanceof Error) {
       return;
     }
 
     dispatch(updateOneEvent(adaptEvent(rawEvent)));
-  }, [doFetchEvent, isUserLoggedIn, dispatch]);
+  }, [doFetchEvent, dispatch]);
 
   return { loading, fetchEvent };
 };
